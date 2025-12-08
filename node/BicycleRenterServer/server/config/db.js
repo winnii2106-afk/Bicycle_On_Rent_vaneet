ce488b30f54fbcc72fe6f16096f4898a2da3c886
Naveenpass1 <naveenpaswan542@gmail.com>
2025-12-08 10:22:42 +0530

    update db.js

--- node/BicycleRenterServer/server/config/db.js
+++ node/BicycleRenterServer/server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://jaspreet:1Jaspreet@student.civhn6j.mongodb.net/bicycleDb'
+const dbPath = 'mongodb+srv://2021appscicse542_db_user:eAUq7bbTakS1qNhM@biycle.4wbi1tj.mongodb.net/bicycleDb'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
     console.log("Db Connected")
